2944cd16a590b6476c0c99880607353db513a050
Sunny2305 <137488522+Sunny2305@users.noreply.github.com>
2023-06-28 17:05:37 +0530

    Add files via upload

---
+++ operator1.py
@@ -0,0 +1,22 @@
+# using Comparison Operators
+a = 7
+
+b = 7
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
